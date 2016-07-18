1a8aa97acb411ea8e967dfaabcde74eaf7bc250f
Xackery Xtal <xackery@gmail.com>
2016-07-18 06:59:18 -0700

    Added a wizard to config, so that user is prompted for user/pass to generate gobeam.config if it does not exist.

--- gobeamconfig.go
+++ gobeamconfig.go
@@ -19,14 +19,48 @@ func (c *GoBeamConfig) Load(path string) (err error) {
 		return
 	}
 
+	fi, err := os.Stat(path)
+	if fi == nil {
+		//write an empty file, since it doesn't exist
+		for {
+			fmt.Printf("Username: ")
+			_, err = fmt.Scanln(&c.Username)
+			if len(c.Username) < 3 {
+				fmt.Println("Username must be greater than 3 characters")
+				continue
+			}
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+			break
+		}
+		for {
+			fmt.Printf("Password: ")
+			_, err = fmt.Scanln(&c.Password)
+			if len(c.Password) < 3 {
+				fmt.Println("Password must be greater than 3 characters")
+				continue
+			}
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+			break
+		}
+		fmt.Println("Saved credentials to", path)
+		err = c.Save(path)
+		return
+	}
+
 	f, err := os.Open(path)
+
 	if err != nil {
 		if err != os.ErrNotExist {
 			err = fmt.Errorf("Error opening config: %s", err.Error())
 			return
 		}
-		//write an empty file, since it doesn't exist
-		c.Save(path)
+		err = fmt.Errorf("Unknown error opening config: %s", err.Error())
 		return
 	}
 	//info, err := f.Stat()

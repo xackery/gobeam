e32a5786696133de7c740045d1e1a10ebe7cf466
Xackery Xtal <xackery@gmail.com>
2016-07-18 07:03:42 -0700

    added examples/chatbot_basic

---
+++ examples/chatbot_basic/chatbot_basic.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	beam "github.com/xackery/gobeam"
+	"log"
+)
+
+func main() {
+	//Create a chatbot session.
+	chatbot := beam.Session{
+		Debug: false,
+	}
+
+	//subscribe to events
+	chatbot.AddHandler(userJoin)
+	chatbot.AddHandler(userLeave)
+	chatbot.AddHandler(userChat)
+
+	//Log in
+	log.Println("[Chatbot] Logging in")
+	err := chatbot.Login()
+	if err != nil {
+		log.Println("[Chatbot] Error logging in:", err.Error())
+		return
+	}
+
+	// Open chatbot websocket
+	log.Println("[Chatbot] Connecting")
+	err = chatbot.Open()
+	if err != nil {
+		log.Println("[Chatbot] Error opening winsock:", err.Error())
+		return
+	}
+	log.Println("[Chatbot] Success!")
+
+	// Simple way to keep program running until any key press.
+	var input string
+	fmt.Scanln(&input)
+	return
+}
+
+func userJoin(s *beam.Session, m *beam.UserJoinEvent) {
+	log.Println("[Chatbot] %s joined the channel.\n", m.Username)
+}
+
+func userLeave(s *beam.Session, m *beam.UserJoinEvent) {
+	log.Printf("[Chatbot] %s left the channel.\n", m.Username)
+}
+
+func userChat(s *beam.Session, m *beam.ChatMessageEvent) {
+	log.Printf("[Chatbot] %s: %s\n", m.Username, m.Message.Messages[0].Text)
+}
